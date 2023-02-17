b061db1cee5509f9efa7a7fc82b93284943f2e1d
alizard <coyotehelp123@gmail.com>
2023-02-17 11:10:00 -0800

    initial file setup finished
    - Basic stylesheet
    - Core stuff to make an app start

--- Basalt/index.js
+++ Basalt/index.js
@@ -1,10 +1,15 @@
+//consts and modules
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 // create window
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js') //points to the path of the currently executing script
+      }
     })
   
     win.loadFile('index.html')
