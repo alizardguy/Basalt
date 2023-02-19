dbe22216e58c488a439fbc3cf89a992329aa2ed7
alizardguy <42082829+alizardguy@users.noreply.github.com>
2023-02-18 16:44:05 -0800

    Dropdown now sorta works
    like it shows up, work "works" is a strong word

--- Basalt/js/dropdown.js
+++ Basalt/js/dropdown.js
@@ -1,15 +1,12 @@
 console.log("dropdown.js loaded");
-/* bar buttons */
-const fileMenu =  document.getElementById('file-menu');
 
 /* dropdown menus */
 const fileMenuDropdown = document.getElementById('file-dropdown-menu');
 
 /* event listeners */
-fileMenu.addEventListener("onclick", showFileDropdown);
+document.getElementById("nav-menu-open").addEventListener("click", showFileDropdown);
 
 /* show functions */
 function showFileDropdown() {
     fileMenuDropdown.hidden = !fileMenuDropdown.hidden;
-    console.log("showFileDropdown() called"); //to do: make this work
 }
